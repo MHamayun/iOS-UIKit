905a7bc988da9a9e7732597c25e92fb1a2db9707
Max Cobb <maxxc@mac.com>
2021-09-09 14:47:06 +0100

    couple other setExtensionProperty methods, for boolean and default string

--- Sources/Agora-UIKit/AgoraVideoViewer+AgoraRtcEngineDelegate.swift
+++ Sources/Agora-UIKit/AgoraVideoViewer+AgoraRtcEngineDelegate.swift
@@ -76,8 +76,6 @@ extension AgoraVideoViewer: AgoraRtcEngineDelegate {
         )
     }
 
-
-
     /**
      Occurs when a remote user (Communication)/host (Live Broadcast) leaves a channel. Same as userOfflineBlock.
 
